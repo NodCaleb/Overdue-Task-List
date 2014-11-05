6ff023843c6ed306b3c2a4a7fda53b12a54b6dd0
Eugene Rozhkov <eugenerozhkov@Teiwaz.local>
2014-11-05 17:42:29 +0200

    Initial Commit

---
+++ Overdue Task List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Overdue Task List
+//
+//  Created by Eugene Rozhkov on 05.11.14.
+//  Copyright (c) 2014 nordPoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Overdue Task List/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Overdue Task List
+//
+//  Created by Eugene Rozhkov on 05.11.14.
+//  Copyright (c) 2014 nordPoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
